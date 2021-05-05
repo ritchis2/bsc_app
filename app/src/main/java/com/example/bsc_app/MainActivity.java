13c43acb1615360b421c48cf008911b94cf315cf
ritchis2 <ritchis2@lsbu.ac.uk>
2021-05-05 20:46:04 +0100

    updated

--- app/src/main/java/com/example/bsc_app/MainActivity.java
+++ app/src/main/java/com/example/bsc_app/MainActivity.java
@@ -6,15 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
 }
--- app/src/main/java/com/example/bsc_app/fragment1.java
+++ app/src/main/java/com/example/bsc_app/fragment1.java
@@ -6,19 +6,4 @@ import android.widget.Button;
 
 public class fragment1 {
 
-    private Button button;
-
-    button = (Button) findViewById(R.id.next1);
-        button.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            nextFragment1();
-        }
-    });
-}
-
-    public void nextFragment1() {
-        Intent intent = new Intent(this, fragment2.xml);
-        startActivity(intent);
-    }
 }
