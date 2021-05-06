4a2ee9bc473a70305e5c420f54037c39af86d95e
ritchis2 <ritchis2@lsbu.ac.uk>
2021-05-06 07:55:20 +0100

    done: login, registration, onboarding functional, nav_bar semi functional to-do: appointment system, message system, email verification, migrate activites to single viewPager for mainActivity instead of separate activites, add info and details

--- app/src/main/java/com/example/bsc_app/MainActivity.java
+++ app/src/main/java/com/example/bsc_app/MainActivity.java
@@ -1,20 +1,64 @@
 package com.example.bsc_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import android.content.Intent;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView userEmail;
+
+    FirebaseAuth mAuth;
+    FirebaseUser user;
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Fetch / Display User
+        userEmail = findViewById(R.id.userDisplay);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
+        userEmail.setText(user.getEmail());
+
+        // NavBar
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.nav_view);
+
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_contact:
+                        startActivity(new Intent(getApplicationContext()
+                        ,Contact.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.navigation_home:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void logout(View view) {
@@ -22,4 +66,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(),Login.class));
         finish();
     }
+
+
 }
